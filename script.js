2e63c4e693ddeb9c86eda297bcea8ef54be0d91f
LeluuChMo <leluuchmorchadze@gmail.com>
2024-07-28 13:33:32 +0300

    created script.js
    added function for cookies

---
+++ script.js
@@ -0,0 +1,11 @@
+setCookie = (cookieName, cookieValue, expDays) => {
+  let date = new Date();
+  date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = `${cookieName}=${cookieValue}; expires=${expires}; path=/; SameSite=Lax; Secure`;
+};
+
+document.querySelector("#cookie-button").addEventListener("click", () => {
+  document.querySelector("#cookie-wrapper").style.display = "none";
+  setCookie("cookie", true, 30);
+});
