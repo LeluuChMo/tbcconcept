e9dfe4f703086d2987f15a717e97b9bebc7603a9
LeluuChMo <leluuchmorchadze@gmail.com>
2024-07-28 19:09:03 +0300

    updated script.js
    added award section functions and "strict"

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+"strict";
+
+// Cookies
 setCookie = (cookieName, cookieValue, expDays) => {
   let date = new Date();
   date.setTime(date.getTime() + expDays * 24 * 60 * 60 * 1000);
@@ -9,3 +12,53 @@ document.querySelector("#cookie-button").addEventListener("click", () => {
   document.querySelector("#cookie-wrapper").style.display = "none";
   setCookie("cookie", true, 30);
 });
+
+// Award Section
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const carouselChildrens = [...carousel.children];
+
+let isDragging = false,
+  isAutoPlay = true,
+  startX,
+  startScrollLeft,
+  timeoutId;
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
+
+carousel.classList.add("no-transition");
+carousel.scrollLeft = 0;
+carousel.classList.remove("no-transition");
+
+arrowBtns.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    carousel.scrollLeft += btn.id == "left" ? -firstCardWidth : firstCardWidth;
+  });
+});
+
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+};
+
+const dragging = (e) => {
+  if (!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+};
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+};
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
